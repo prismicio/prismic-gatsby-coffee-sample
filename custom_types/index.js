6c723be926da5ab1b94ca045a8b86ccb70212662
Raul Gonzalez <raulg42@gmail.com>
2019-04-11 15:45:22 +0200

    Custom Types

---
+++ custom_types/index.js
@@ -0,0 +1,41 @@
+[{
+  "id": "author",
+  "name": "Author",
+  "repeatable": true,
+  "value": "author.json"
+},{
+  "id": "blog_home",
+  "name": "Blog Home Metadata",
+  "repeatable": false,
+  "value": "blog_home.json"
+},{
+  "id": "blog_post",
+  "name": "Blog post",
+  "repeatable": true,
+  "value": "blog_post.json"
+},{
+  "id": "homepage",
+  "name": "Homepage",
+  "repeatable": false,
+  "value": "homepage.json"
+},{
+  "id": "landing_page",
+  "name": "Landing Page",
+  "repeatable": true,
+  "value": "landing_page.json"
+},{
+  "id": "layout",
+  "name": "Layout",
+  "repeatable": false,
+  "value": "layout.json"
+},{
+  "id": "product",
+  "name": "Product",
+  "repeatable": true,
+  "value": "product.json"
+},{
+  "id": "products",
+  "name": "Products List",
+  "repeatable": true,
+  "value": "products.json"
+}]
