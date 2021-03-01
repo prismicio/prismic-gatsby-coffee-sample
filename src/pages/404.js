e28905238620b0bd5bfee9baf28648af8f1f84da
PaulinaVPG <pvpg@live.com.mx>
2021-03-01 17:06:43 -0600

    commented out 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { withUnpublishedPreview } from 'gatsby-source-prismic'
-import { BlogPostTemplate } from '../templates/BlogPost'
-// import { ProductTemplate } from '../templates/Product'
+// import React from 'react'
+// import { withUnpublishedPreview } from 'gatsby-source-prismic'
+// import { BlogPostTemplate } from '../templates/BlogPost'
+// // import { ProductTemplate } from '../templates/Product'
 
-const Page404 = () => (
-  <div className="not-found">
-    <h1>404</h1>
-    <h3>The page you are looking for was not found</h3>
-    <p>
-      <a href="/">
-        <button type="button">Return to homepage</button>
-      </a>
-    </p>
-  </div>
-)
+// const Page404 = () => (
+//   <div className="not-found">
+//     <h1>404</h1>
+//     <h3>The page you are looking for was not found</h3>
+//     <p>
+//       <a href="/">
+//         <button type="button">Return to homepage</button>
+//       </a>
+//     </p>
+//   </div>
+// )
 
-export default withUnpublishedPreview(Page404, {
-  templateMap: {
-    // product: ProductTemplate,
-    blog_post: BlogPostTemplate,
-    // prismicProduct: ProductTemplate,
-    prismicBlogPost: BlogPostTemplate,
-  },
-})
+// export default withUnpublishedPreview(Page404, {
+//   templateMap: {
+//     // product: ProductTemplate,
+//     blog_post: BlogPostTemplate,
+//     // prismicProduct: ProductTemplate,
+//     prismicBlogPost: BlogPostTemplate,
+//   },
+// })
